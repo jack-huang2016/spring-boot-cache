ceebd016925e7885c4b9b21e18f7387d5fa15d94
jack-huang2016 <2456120975@qq.com>
2019-11-18 15:07:44 +0800

    Springboot整合Cache缓存

---
+++ springboot-cache-web/src/main/java/com/sample/test/SpringbootCacheWebApplication.java
@@ -0,0 +1,13 @@
+package com.sample.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCacheWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCacheWebApplication.class, args);
+    }
+
+}
